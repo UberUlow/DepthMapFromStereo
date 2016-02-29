f56314e065b951d4756650001f52d0b90f25d7e3
Poleychuk Vladislav <uberulow@gmail.com>
2016-02-29 11:05:15 +0300

    Project created.

---
+++ DepthMapFromStereo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepthMapFromStereo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
