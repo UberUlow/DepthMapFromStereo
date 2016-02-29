4b5c154eca108d3ef4fe442cf5055d389e6f4717
Poleychuk Vladislav <uberulow@gmail.com>
2016-02-29 11:16:15 +0300

    Add class Pair and Xml writer and reader.

---
+++ DepthMapFromStereo/Pair.cs
@@ -0,0 +1,76 @@
+﻿namespace DepthMapFromStereo
+{
+    /// <summary>
+    /// Пара изображений с параметрами камеры и размером шаблона
+    /// </summary>
+    public class Pair
+    {
+        public Image Image1; // Изображение 1
+        public Image Image2; // Изображение 2
+        public Settings Properties; // Параметры камеры
+
+        public Pair() { }
+
+        public Pair(Image image1, Image image2, Settings properties)
+        {
+            Image1 = image1;
+            Image2 = image2;
+            Properties = properties;
+        }
+
+        /// <summary>
+        /// Изображение
+        /// </summary>
+        public class Image
+        {
+            public string Path; // Путь к файлу
+            public Coordinates CoordinatesCamera; // Координаты камеры в момент съемки
+
+            public Image() { }
+
+            public Image(string path, Coordinates coordinatesCamera)
+            {
+                Path = path;
+                CoordinatesCamera = coordinatesCamera;
+            }
+
+            /// <summary>
+            /// Координаты камеры в момент съемки
+            /// </summary>
+            public class Coordinates
+            {
+                public int X; // Координата X
+                public int Y; // Координата Y
+                public int Z; // Координата Z
+
+                public Coordinates() { }
+
+                public Coordinates(int x, int y, int z)
+                {
+                    X = x;
+                    Y = y;
+                    Z = z;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Параметры камеры и размер шаблона дял поиска
+        /// </summary>
+        public class Settings
+        {
+            public double FocalLength; // Фокусное расстояние
+            public double Distance; // Расстояние между камерами
+            public int TemplateSize; // Размер шаблона
+
+            public Settings() { }
+
+            public Settings(double focalLength, double distance, int templateSize)
+            {
+                FocalLength = focalLength;
+                Distance = distance;
+                TemplateSize = templateSize;
+            }
+        }
+    }
+}
---
+++ DepthMapFromStereo/Xml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace DepthMapFromStereo
+{
+    /// <summary>
+    /// Класс чтения и записи любых классов в XML-файл
+    /// </summary>
+    public static class XML
+    {
+        /// <summary>
+        /// Import XML
+        /// </summary>
+        /// <param name="fileName">Имя файла</param>
+        /// <returns>Список классов</returns>
+        public static List<T> Import<T>(string fileName)
+        {
+            try
+            {
+                List<T> objects = new List<T>();
+                XmlSerializer srzr = new XmlSerializer(typeof(List<T>));
+                StreamReader reader = new StreamReader(fileName);
+                objects = (List<T>)srzr.Deserialize(reader);
+                reader.Close();
+                Console.WriteLine("Данные импортированы из XML-файла.\n");
+                return objects;
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка чтения данных из XML-файла. Повторите попытку.");
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Export XML
+        /// </summary>
+        /// <param name="objects">Список классов</param>
+        /// <param name="fileName">Имя файла</param>
+        public static void Write<T>(List<T> objects, string fileName)
+        {
+            XmlSerializer srzr = new XmlSerializer(typeof(List<T>));
+            try
+            {
+                StreamWriter sw = new StreamWriter(fileName);
+                srzr.Serialize(sw, objects);
+                sw.Close();
+                Console.WriteLine("Данные экспортированы в XML-файл\n");
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка записи данных в файл.");
+            }
+        }
+    }
+}
